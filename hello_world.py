fa34471201defbb8b6d31b25d23fe72f23dc8c43
akai <1296282494@qq.com>
2020-04-26 01:59:12 +0800

    hellow_world

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+class HelloWorld(object):
+	"""docstring for HelloWorld"""
+	def __init__(self, arg):
+		super(HelloWorld, self).__init__()
+		self.arg = arg
+	def run():
+		print("HelloWorld")
+		return None
+
+if __name__ == '__main__':
+	hw = HelloWorld()
+	hw.run()
+
+
+		
